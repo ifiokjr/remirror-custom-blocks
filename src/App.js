35accc066e6754a1bdb28eede17fb03680ba24b3
Tom Moor <tom@abstract.com>
2020-07-20 14:17:49 -0700

    repro

--- src/App.js
+++ src/App.js
@@ -1,35 +1,103 @@
-import React, { useCallback } from 'react';
-import { BoldExtension } from 'remirror/extension/bold';
-import { RemirrorProvider, useManager, useRemirror } from 'remirror/react';
-import CustomBlockExtension from "./CustomBlockExtension";
-import { ReactComponentExtension } from "@remirror/extension-react-component";
+import React, { FC, useState, useCallback } from "react";
+import { BoldExtension } from "remirror/extension/bold";
+import { ItalicExtension } from "remirror/extension/italic";
+import { HeadingExtension } from "remirror/extension/heading";
+import { ListPreset } from "remirror/preset/list";
+import { RemirrorProvider, useManager, useRemirror, usePositioner } from "remirror/react";
+const EXTENSIONS = [
+  new HeadingExtension(),
+  new BoldExtension(),
+  new ItalicExtension(),
+  new ListPreset(),
+];
 
-const Editor = () => {
-  const { getRootProps, commands } = useRemirror();
+function Menu() {
+    const [activeCommands, setActiveCommands] = useState({
+      bold: false,
+      italic: false,
+      underline: false,
+    });
 
-  const toggleCustomBlock = useCallback(() => {
-    commands.toggleCustomBlock();
-  }, [commands]);
+    // The use positioner hook allows for tracking the current selection in the editor.
+    const { top, left, ref } = usePositioner('bubble');
 
+    const { commands, active } = useRemirror(() => {
+      setActiveCommands({
+        bold: active.bold(),
+        italic: active.italic(),
+        underline: active.underline(),
+      });
+    });
+
+    return (
+      <div ref={ref} style={{ top, left, position: 'absolute', }}>
+        <button
+          onClick={() => commands.toggleBold()}
+          style={{ fontWeight: activeCommands.bold ? 'bold' : undefined }}
+        >
+          B
+        </button>
+        <button
+          onClick={() => commands.toggleItalic()}
+          style={{ fontWeight: activeCommands.italic ? 'bold' : undefined }}
+        >
+          I
+        </button>
+        <button
+          onClick={() => commands.toggleUnderline()}
+          style={{ fontWeight: activeCommands.underline ? 'bold' : undefined }}
+        >
+          U
+        </button>
+      </div>
+    );
+  }
+
+function usePersistedValue(manager) {
+  const [value, setValue] = useState(
+    manager.createState({
+      content: JSON.parse(localStorage.getItem("saved"))
+    })
+  );
+
+  const onChange = useCallback((event) => {
+    if (event.tr && event.tr.docChanged) {
+      // Persist state to localStorage for demo
+      const saved = event.state.doc.toJSON();
+      localStorage.setItem("saved", JSON.stringify(saved));
+    }
+    
+    setValue(event.state);
+  }, []);
+
+  return { value, onChange }
+}
+
+/**
+ * This component contains the editor and any toolbars/chrome it requires.
+ */
+const SmallEditor: FC = () => {
+  const { getRootProps } = useRemirror();
   return (
     <div>
       <div {...getRootProps()} />
-      <button onClick={toggleCustomBlock}>
-        Toggle Custom Block
-      </button>
+      <Menu />
     </div>
   );
 };
-
-const App = () => {
-  const manager = useManager([new BoldExtension(), new CustomBlockExtension(), new ReactComponentExtension()]);
+const SmallEditorWrapper = () => {
+  const extensionManager = useManager(EXTENSIONS);
+  const { value, onChange } = usePersistedValue(extensionManager);
 
   return (
-    <RemirrorProvider manager={manager}>
-      <Editor />
+    <RemirrorProvider
+      manager={extensionManager}
+      value={value}
+      onChange={onChange}
+    >
+      <SmallEditor />
     </RemirrorProvider>
   );
 };
 
-export default App
-;
+export default SmallEditorWrapper;
